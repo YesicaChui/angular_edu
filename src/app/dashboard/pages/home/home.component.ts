9956ddcf1c822c7f5e62503e38ca6d3d567b8f73
YesicaChui <yesicachuic@gmail.com>
2023-10-21 12:21:30 -0500

    agregando observables

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { User } from '../users/models';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy { 
+
+  clocksSubscription: Subscription
   constructor() {
     const getUsersPromise = new Promise((resolve, reject)=>{
       const users :  User[] = [
@@ -34,5 +37,26 @@ export class HomeComponent {
       })
 
       console.log('holaaa')
+
+      this.clocksSubscription = this.getClock().subscribe({
+        next: (tiempo)=>{
+          console.log('que pasara aqui', tiempo)
+        },       
+
+      })
+      
+      
+  }
+  ngOnDestroy(): void {
+   this.clocksSubscription.unsubscribe()
+    
+  }
+  getClock(): Observable<Date>{
+    return new Observable((suscriber)=>{
+      setInterval(()=>{
+        suscriber.next(new Date())
+        
+      }, 1000)
+    })
   }
 }
